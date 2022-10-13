16598b0f9bd6120f02518bcd9863b006fabfc237
Nitish <nitishrajbongshi@gmail.com>
2022-10-14 00:11:00 +0530

    Update the current file.

--- student/student_profile.php
+++ student/student_profile.php
@@ -14,7 +14,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
     <!-- ===== BOX ICONS ===== -->
     <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
@@ -24,7 +25,8 @@
     <!-- google font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=PT+Serif&family=Ubuntu:wght@300;500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif&family=Ubuntu:wght@300;500&display=swap"
+        rel="stylesheet">
 
     <!-- ===== CSS ===== -->
     <link rel="stylesheet" href="../style/profile_sidebar.css">
@@ -39,26 +41,23 @@
 <body id="body-pd">
     <?php
         include "partials/_navbar.php";
+        include "changes/change_password.php";
 
         // logout and redirect to index page
         if(isset($_POST['logout'])) {
             header("location: student_logout.php");
             exit;
         }
+
     ?>
 
     <!-- change password -->
     <div class="cp">
         <div class="popup-content">
-            <h2>Change password</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore
-                magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
-                id
-                laborum.</p>
+            <div class="container">
+                <h2>Change password</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis totam qui mollitia velit soluta eius nulla tenetur eaque, reiciendis quia!</p>
+            </div>
             <a class="closeBtn" href="javascript:void(0)">x</a>
         </div>
     </div>
@@ -68,12 +67,19 @@
     font-family: 'Ubuntu', sans-serif;">
         <div class="row featurette pt-3">
             <div class="col-md-5 order-md-1 d-flex">
-                <img class="flex-shrink-0 bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="300" height="200" src="../photo/profile_student.png" alt="Student profile">
+                <img class="flex-shrink-0 bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+                    width="300" height="200" src="../photo/profile_student.png" alt="Student profile">
             </div>
             <div class="col-md-7 order-md-2">
-                <h4 class="text-dark text-sm-left pt-4">Hello, <?php echo $_SESSION['username']; ?></h4>
-                <p class="lead mb-1">Email: <?php echo $_SESSION['email']; ?></p>
-                <p class="lead mb-1">Mobile: <?php echo $_SESSION['phone']; ?></p>
+                <h4 class="text-dark text-sm-left pt-4">Hello,
+                    <?php echo $_SESSION['username']; ?>
+                </h4>
+                <p class="lead mb-1">Email:
+                    <?php echo $_SESSION['email']; ?>
+                </p>
+                <p class="lead mb-1">Mobile:
+                    <?php echo $_SESSION['phone']; ?>
+                </p>
                 <p class="lead mb-1">Depertment: CSE</p>
                 <p class="lead mb-1">Program: MCA</p>
                 <div class="chng_btn">
@@ -107,11 +113,13 @@
     <script src="../script/jquery.js"></script>
     <script src="../script/profile_sidebar.js"></script>
     <script src="../script/change_password.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+        crossorigin="anonymous"></script>
     <script>
-        $(document).ready(function() {
+        $(document).ready(function () {
             // toggle mentor's details
-            $("#mentor_details").click(function() {
+            $("#mentor_details").click(function () {
                 $("#mentor_detail_show").slideToggle(1100);
             })
 
